3e9d9bd4b3c61ef70a4328dc480d2b79004cda4d
Monzur Alam <46376609+monzuralam@users.noreply.github.com>
2026-04-22 12:20:21 +0600

    Apply suggestions from code review
    Co-authored-by: Scott Kingsley Clark <scott@skc.dev>
    Signed-off-by: Monzur Alam <46376609+monzuralam@users.noreply.github.com>

--- classes/PodsInit.php
+++ classes/PodsInit.php
@@ -160,7 +160,7 @@ class PodsInit {
 		add_action( 'plugins_loaded', [ $this, 'activate_install' ], 9 );
 		add_action( 'after_setup_theme', [ $this, 'after_setup_theme' ] );
 		add_action( 'wp_loaded', [ $this, 'flush_rewrite_rules' ] );
-		add_filter( 'plugin_action_links_' . plugin_basename( PODS_FILE ), [ $this, 'settings_link' ] );
+		add_filter( 'plugin_action_links_' . PODS_SLUG, [ $this, 'settings_link' ] );
 	}
 
 	/**
@@ -1909,19 +1909,32 @@ class PodsInit {
 	}
 
 	/**
-	 * Add Pods settings link
+	 * Add custom action links for Pods.
 	 * 
-	 * @param array $links
+	 * @param string[] $actions An array of plugin action links.
 	 * 
-	 * @return array
-	 * @since 3.3.8
-	 * @author monzuralam
+	 * @return string[] An array of plugin action links.
+	 * @since TBD
 	 **/
-	public function settings_link( $links ) {
-        $links['pods_settings'] = sprintf( '<a href="%s">%s</a>', admin_url( 'admin.php?page=pods-settings' ), esc_html__( 'Settings', 'pods' ) );
+	public function settings_link( array $links ): array {
+		// Check if the Pods admin menu is disabled.
+		if ( defined( 'PODS_DISABLE_ADMIN_MENU' ) && ! PODS_DISABLE_ADMIN_MENU ) {
+			return $links;
+		}
 
-        return $links;
-    }
+		// Check if user has access to the Pods Settings page.
+		if ( ! pods_is_admin( 'pods_settings' ) ) {
+			return $links;
+		}
+
+		$links['pods_settings'] = sprintf(
+			'<a href="%s">%s</a>',
+			esc_url( admin_url( 'admin.php?page=pods-settings' ) ),
+			esc_html__( 'Settings', 'pods' )
+		);
+
+		return $links;
+	}
 
 	/**
 	 * Update Post Type messages
